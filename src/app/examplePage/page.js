8b1bc29c44d04f704c69b98614e0911c99a29959
Andrew Banagas <andrewbanagas40@gmail.com>
2023-12-12 19:57:19 -0800

    Andrew's Update

---
+++ src/app/examplePage/page.js
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <div>
+      <h1>Hello! You're in example page</h1>
+      <a href="/">
+        <h2>I am a link back to home</h2>
+      </a>
+    </div>
+  );
+}
